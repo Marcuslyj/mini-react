7fd394ef83628d0202aadf73e6bdabb49c887c85
Marcus <814712074@qq.com>
2022-03-24 19:11:14 +0800

    fibers

--- readme.md
+++ readme.md
@@ -28,4 +28,10 @@ use requestIdleCallback to keep looping.
 To start using the loop we’ll need to set the first unit of work, and then write a performUnitOfWork function that not only performs the work but also returns the next unit of work.
 
 
+Fibers:
+To organize the units of work we’ll need a data structure: a fiber tree.
+We’ll have one fiber for each element and each fiber will be a unit of work.
+
+One of the goals of this data structure is to make it easy to find the next unit of work. That’s why each fiber has a link to its first child, its next sibling and its parent.
+
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// To start using the loop we’ll need to set the first unit of work,
+// and then write a performUnitOfWork function that not only performs the work but also returns the next unit of work.
+let nextUnitOfWork = null
+
 // 处理基础数据类型
 function createTextElement(text) {
     return {
@@ -25,31 +29,35 @@ function createElement(type, props, ...children) {
     }
 }
 
-function render(element, container) {
+// fiber就类似一个vnode
+function createDom(fiber) {
     const dom =
         element.type == "TEXT_ELEMENT"
             ? document.createTextNode("")
-            : document.createElement(element.type)
+            : document.createElement(fiber.type)
 
     const isProperty = key => key !== "children"
-    Object.keys(element.props)
+    Object.keys(fiber.props)
         .filter(isProperty)
         .forEach(name => {
-            dom[name] = element.props[name]
+            dom[name] = fiber.props[name]
         })
 
-    element.props.children.forEach(child =>
-        render(child, dom)
-    )
-    container.appendChild(dom)
+    return dom
+}
+
+// In the render function we set nextUnitOfWork to the root of the fiber tree.
+// 起点吧，这样requestIdleCallback(workLoop)就开始工作了
+function render(element, container) {
+    nextUnitOfWork = {
+        dom: container,
+        props: {
+            children: [element],
+        },
+    }
 }
 
 // break the work into small units
-
-// To start using the loop we’ll need to set the first unit of work,
-// and then write a performUnitOfWork function that not only performs the work but also returns the next unit of work.
-let nextUnitOfWork = null
-
 function workLoop(deadline) {
     let shouldYield = false
     while (nextUnitOfWork && !shouldYield) {
@@ -64,8 +72,54 @@ function workLoop(deadline) {
 // workloop一直保持运行
 requestIdleCallback(workLoop)
 
-function performUnitOfWork(nextUnitOfWork) {
-    // TODO
+function performUnitOfWork(fiber) {
+    // TODO add dom node
+    if (!fiber.dom) {
+        // 创建dom
+        fiber.dom = createDom(fiber)
+    }
+
+    if (fiber.parent) {
+        // append to parent
+        fiber.parent.dom.appendChild(fiber.dom)
+    }
+
+    // TODO create new fibers
+    const elements = fiber.props.children
+    let index = 0
+    let prevSibling = null
+
+    while (index < elements.length) {
+        const element = elements[index]
+
+        const newFiber = {
+            type: element.type,
+            props: element.props,
+            parent: fiber,
+            dom: null,
+        }
+
+        if (index === 0) {
+            fiber.child = newFiber
+        } else {
+            prevSibling.sibling = newFiber
+        }
+
+        prevSibling = newFiber
+        index++
+    }
+
+    if (fiber.child) {
+        return fiber.child
+    }
+    let nextFiber = fiber
+    while (nextFiber) {
+        if (nextFiber.sibling) {
+            return nextFiber.sibling
+        }
+        nextFiber = nextFiber.parent
+    }
+    // TODO return next unit of work
 }
 
 const Didact = {
