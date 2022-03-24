684f38794894e8a5cac88e16ce3be39eb5266ff5
Marcus <814712074@qq.com>
2022-03-24 15:43:05 +0800

    feat：Step I: The createElement Function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 import React from 'react';
 
-const element = React.createElement(
-    "div",
-    { id: "foo" },
-    React.createElement("a", null, "bar"),
-    React.createElement("b")
+// 处理基础数据类型
+function createTextElement(text) {
+    return {
+        type: "TEXT_ELEMENT",
+        props: {
+            nodeValue: text,
+            children: [],
+        },
+    }
+}
+
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map(child =>
+                typeof child === "object"
+                    ? child
+                    : createTextElement(child)
+            ),
+        }
+    }
+}
+
+const Didact = {
+    createElement
+}
+
+// const element = Didact.createElement(
+//     "div",
+//     { id: "foo" },
+//     Didact.createElement("a", null, "bar"),
+//     Didact.createElement("b")
+// )
+
+/** @jsx Didact.createElement */
+const element = (
+    <div id="foo">
+        <a>bar</a>
+        <b />
+    </div>
 )
+
 
 console.log(element);
